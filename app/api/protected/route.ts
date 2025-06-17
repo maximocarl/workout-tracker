796959216dfe11bfb9d6d1473576fefc1373f3bd
Carl-Justine Maximo <107338477+maximocarl@users.noreply.github.com>
2025-06-16 17:08:04 -0700

    esenlist take 2

--- app/api/protected/route.ts
+++ app/api/protected/route.ts
@@ -1,6 +1,6 @@
 import { getServerSession } from "next-auth";
 
-export async function GET(request: Request) {
+export async function GET(_request: Request) {
   const session = await getServerSession();
   if (!session) {
     return new Response("Unauthorized", { status: 401 });
--- lib/components/SessionProvider.tsx
+++ lib/components/SessionProvider.tsx
@@ -2,13 +2,14 @@
 
 import { SessionProvider } from "next-auth/react";
 import { ReactNode } from "react";
+import type { Session } from "next-auth";
 
 export default function SessionWrapper({
   children,
   session,
 }: {
   children: ReactNode;
-  session: any;
+  session: Session | null;
 }) {
   return <SessionProvider session={session}>{children}</SessionProvider>;
 }
