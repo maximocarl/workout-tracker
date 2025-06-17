647c54db4c09c46ee4e7a9f512d1c792d51afece
Carl-Justine Maximo <107338477+maximocarl@users.noreply.github.com>
2025-06-16 17:25:40 -0700

    esenlist 4

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,29 +1,7 @@
 
 import NextAuth from "next-auth";
-import GoogleProvider from "next-auth/providers/google";
-import GitHubProvider from "next-auth/providers/github";
-import DiscordProvider from "next-auth/providers/discord";
 
-
-const authOptions = {
-  providers: [
-    GoogleProvider({
-      clientId: process.env.GOOGLE_CLIENT_ID ?? "",
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET ?? "",
-    }),
-    GitHubProvider({
-      clientId: process.env.GITHUB_CLIENT_ID ?? "",
-      clientSecret: process.env.GITHUB_CLIENT_SECRET ?? "",
-    }),
-    DiscordProvider({
-      clientId: process.env.DISCORD_CLIENT_ID ?? "",
-      clientSecret: process.env.DISCORD_CLIENT_SECRET ?? "",
-    })
-  ],
-
-  secret: process.env.NEXTAUTH_SECRET,
-};
-
+import { authOptions } from "@/lib/auth";
 
 const handler = NextAuth(authOptions);
 
---
+++ lib/auth.ts
@@ -0,0 +1,26 @@
+import { NextAuthOptions } from "next-auth";
+import GoogleProvider from "next-auth/providers/google";
+import GitHubProvider from "next-auth/providers/github";
+import DiscordProvider from "next-auth/providers/discord";
+
+
+const authOptions: NextAuthOptions = {
+    providers: [
+        GoogleProvider({
+            clientId: process.env.GOOGLE_CLIENT_ID ?? "",
+            clientSecret: process.env.GOOGLE_CLIENT_SECRET ?? "",
+        }),
+        GitHubProvider({
+            clientId: process.env.GITHUB_CLIENT_ID ?? "",
+            clientSecret: process.env.GITHUB_CLIENT_SECRET ?? "",
+        }),
+        DiscordProvider({
+            clientId: process.env.DISCORD_CLIENT_ID ?? "",
+            clientSecret: process.env.DISCORD_CLIENT_SECRET ?? "",
+        })
+    ],
+
+    secret: process.env.NEXTAUTH_SECRET,
+};
+
+export { authOptions };
