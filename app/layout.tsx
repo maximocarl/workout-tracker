7c6bc69186dc3734e2a3317937ea0dcea35582af
Carl-Justine Maximo <107338477+maximocarl@users.noreply.github.com>
2025-06-16 17:43:40 -0700

    fix?

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import "./globals.css";
 import Nav from "../lib/components/nav";
 import { getServerSession } from "next-auth";
-import { authOptions } from "./api/auth/[...nextauth]/route";
+import { authOptions } from "@/lib/auth";
 import SessionWrapper from "../lib/components/SessionProvider";
 
 export default async function RootLayout({ children }: { children: React.ReactNode }) {
