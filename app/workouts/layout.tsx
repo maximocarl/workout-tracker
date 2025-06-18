caf69b2b8043c719b6855453644930e864db95c3
Carl-Justine Maximo <107338477+maximocarl@users.noreply.github.com>
2025-06-18 01:21:27 -0700

    eslist fix again

--- app/workouts/layout.tsx
+++ app/workouts/layout.tsx
@@ -1,15 +1,15 @@
 import { getServerSession } from "next-auth";
 
 export default async function Page({ children }: { children: React.ReactNode }) {
-    // const session = await getServerSession();
-    // if (!session) {
-    //     return (
-    //         <div>
-    //             <h1 className="text-2xl m-2">This is the Workouts page.</h1>
-    //             <h2 className="text-xl m-2">This page only available once logged in.</h2>
-    //         </div>
-    //     );
-    // }
+    const session = await getServerSession();
+    if (!session) {
+        return (
+            <div>
+                <h1 className="text-2xl m-2">This is the Workouts page.</h1>
+                <h2 className="text-xl m-2">This page only available once logged in.</h2>
+            </div>
+        );
+    }
     return (
         <div className="m-2">
             {children}
