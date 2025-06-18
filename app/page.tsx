8e61dab98df2c8ef5f500c9836f763c1b188b247
Carl-Justine Maximo <107338477+maximocarl@users.noreply.github.com>
2025-06-18 15:17:34 -0700

    eslist fix?

--- app/page.tsx
+++ app/page.tsx
@@ -2,18 +2,18 @@ import { DayCard } from "@/lib/components/dayCard";
 import { getServerSession } from "next-auth";
 
 export default async function Home() {
-  // const session = await getServerSession();
+  const session = await getServerSession();
 
-  // if (!session) {
-  //   return (
-  //     <div>
-  //       <h1 className="text-2xl m-2">This is the Home page.</h1>
-  //       <h2 className="text-xl m-2">Please sign in to view this page.</h2>
-  //     </div>
+  if (!session) {
+    return (
+      <div>
+        <h1 className="text-2xl m-2">This is the Home page.</h1>
+        <h2 className="text-xl m-2">Please sign in to view this page.</h2>
+      </div>
 
 
-  //   );
-  // }
+    );
+  }
 
   return (
     <DayCard />
