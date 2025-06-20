86b543db1cc08a86298af62ec4b7012697717bb6
Carl-Justine Maximo <107338477+maximocarl@users.noreply.github.com>
2025-06-20 09:22:08 -0700

    revert

--- app/[id]/edit/page.tsx
+++ app/[id]/edit/page.tsx
@@ -4,6 +4,7 @@ import React, { useEffect, useState } from "react";
 import { Button, Form, Input, Select } from 'antd';
 import { useRouter } from 'next/navigation';
 
+// Type 
 type Workout = {
     _id: string;
     name: string;
